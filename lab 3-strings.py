9f32722b75142e63617b014b63e09f67c57f5c73
Eliran Yahav <elirany22@gmail.com>
2020-08-02 10:14:22 +0300

    checking

--- lab 3-strings.py
+++ lab 3-strings.py
@@ -5,3 +5,4 @@ print("My name is: "+name +"\nMy age is: " +str(age) +"\nMy mail is: "+mail)
 print("My name in reverse is: " +name[::-1]+"\n"+"My fake age: "+str(age*3))
 print ("Eliran" in "idan ben dudu yuval shimon yael galadam shahar yana")
 
+print("Bye Bye....")
