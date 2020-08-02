ae084dbd25cc6d5e5f427a678d4b2d859796b8b8
eliranyahav <68807431+eliranyahav@users.noreply.github.com>
2020-08-02 10:09:10 +0300

    Add files via upload

---
+++ lab 3-strings.py
@@ -0,0 +1,7 @@
+name="Eliran"
+mail="eliran@gmail.com"
+age=35
+print("My name is: "+name +"\nMy age is: " +str(age) +"\nMy mail is: "+mail)
+print("My name in reverse is: " +name[::-1]+"\n"+"My fake age: "+str(age*3))
+print ("Eliran" in "idan ben dudu yuval shimon yael galadam shahar yana")
+
